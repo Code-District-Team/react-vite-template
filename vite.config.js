803f42ff83c245105a85846756ff834c262c3e43
Usama Mehmood <usama.mehmood@codedistrict.com>
2023-05-15 22:34:45 +0500

    Add absolute path config, linter config, define server config in env

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,26 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
+import eslint from "vite-plugin-eslint";
+import { resolve } from "path";
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+
+  return {
+    define: {
+      __APP_ENV__: env.APP_ENV,
+    },
+    plugins: [react(), eslint()],
+    resolve: {
+      alias: {
+        "~": resolve(__dirname, "./src"),
+      },
+    },
+    server: {
+      open: true,
+      // https: true,
+      host: env.VITE_CLIENT_BASE_HOST,
+      port: env.VITE_CLIENT_BASE_PORT,
+    },
+  };
+});
