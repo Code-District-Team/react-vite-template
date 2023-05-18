107c16d53d7e2069b23d722d0f4cc19d1c8fe48a
Usama Mehmood <usama.mehmood@codedistrict.com>
2023-05-18 12:54:02 +0500

    Add disable date functions

--- src/utilities/dateUtility.js
+++ src/utilities/dateUtility.js
@@ -1,1 +1,11 @@
 import moment from "moment";
+
+// * Can not select days before today and today
+export const disabledPastDate = (current) =>
+  current && current < moment().startOf("day");
+
+export const disableFutureDate = (current) =>
+  current && current > moment().endOf("day");
+
+export const disabledDatesPriorMonth = (current) =>
+  current && current < moment().subtract(1, "month").startOf("month");
