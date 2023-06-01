eba05cdc76c78d8edc829e6a44b90c5193308e19
Usama Mehmood <usama.mehmood@codedistrict.com>
2023-06-01 17:26:56 +0500

    Rename root div id

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import App from "./app/App";
 import store from "./app/store";
 import "./styles.scss";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById("appMountPoint")).render(
   <StrictMode>
     <Provider store={store}>
       <App />
