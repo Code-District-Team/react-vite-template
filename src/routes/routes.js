2a07c3a0730c54833796d26864d174b99713eadf
Usama Mehmood <usama.mehmood@codedistrict.com>
2023-06-01 19:40:25 +0500

    Working on routing

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -27,8 +27,12 @@ import K from "~/utilities/constants";
 */
 
 const defaultCrudChildren = [
-  { path: "/details/:id", name: "Details" },
-  { path: "/store/:id", name: "Edit" },
+  {
+    path: "store/create",
+    name: "Create",
+    // component: CreateProjects,
+    // layout: LoggedInPageLayout,
+  },
 ];
 
 const routes = [
@@ -62,7 +66,6 @@ const routes = [
     component: Projects,
     authenticated: true,
     permission: [K.Permissions.Admin],
-    exact: true,
     children: defaultCrudChildren, // TODO: need to implement children functionality in routes
     layout: LoggedInPageLayout,
   },
@@ -72,13 +75,11 @@ const routes = [
     component: Users,
     authenticated: true,
     permission: [],
-    exact: true,
     layout: LoggedInPageLayout,
   },
   {
     path: "/",
     name: "Dashboard",
-    exact: true,
     component: Dashboard,
     authenticated: true,
     layout: LoggedInPageLayout,
@@ -86,7 +87,6 @@ const routes = [
   {
     path: "/counter",
     name: "Counter",
-    exact: true,
     component: Counter,
     layout: LoggedInPageLayout,
   },
