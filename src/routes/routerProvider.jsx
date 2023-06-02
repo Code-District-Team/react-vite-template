dcbbb56e8f9ed750514f53ce3cae1f8ae00e259d
Usama Mehmood <usama.mehmood@codedistrict.com>
2023-06-02 13:49:10 +0500

    Remove comment

--- src/routes/routerProvider.jsx
+++ src/routes/routerProvider.jsx
@@ -28,45 +28,6 @@ const router = createBrowserRouter(
   )
 );
 
-/* const router2 = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path="/">
-      <Route index element={<Dashboard />} />
-      <Route path="users" element={<Users />} />
-      <Route path="projects">
-        <Route index element={<Projects />} />
-        <Route path="create" element={<CreateProjects />} />
-      </Route>
-      <Route path="*" element={<NotFound />} />
-    </Route>
-  )
-);
-console.log(router2); */
-
 export default function RouterProvider() {
   return <Provider router={router} />;
 }
-
-/*
-{route?.children?.map((childRoute) => {
-            return (
-              <Route
-                key={childRoute.path}
-                element={<RouteWithSubRoutes route={childRoute} />}
-              >
-                <Route
-                  path={childRoute.path}
-                  element={
-                    childRoute.layout ? (
-                      <childRoute.layout>
-                        <childRoute.component route={route} />
-                      </childRoute.layout>
-                    ) : (
-                      <childRoute.component route={route} />
-                    )
-                  }
-                />
-              </Route>
-            );
-          })}
- */
