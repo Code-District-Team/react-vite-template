e51ed9b15cabf68ac6c39fa86f2ba78e33acafea
Usama Mehmood <usama.mehmood@codedistrict.com>
2023-06-10 12:46:53 +0500

    Wrapped error boundary on every route

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,13 +1,10 @@
-import ErrorBoundary from "~/common/errorBoundary/errorBoundary";
 import ThemeProvider from "~/features/theme/themeProvider";
 import RouterProvider from "~/routes/routerProvider";
 
 export default function App() {
   return (
-    <ErrorBoundary>
-      <ThemeProvider>
-        <RouterProvider />
-      </ThemeProvider>
-    </ErrorBoundary>
+    <ThemeProvider>
+      <RouterProvider />
+    </ThemeProvider>
   );
 }
