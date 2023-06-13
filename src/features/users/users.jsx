d1ae9deff799a2f0b094fe854cd5ace2e198e1d4
Usama Mehmood <usama.mehmood@codedistrict.com>
2023-06-13 15:58:33 +0500

    Minor changes

--- src/features/users/users.jsx
+++ src/features/users/users.jsx
@@ -1,5 +1,10 @@
+import { Card } from "antd";
 import TableList from "~/common/tableList/tableList";
 
 export default function User() {
-  return <TableList />;
+  return (
+    <Card>
+      <TableList />
+    </Card>
+  );
 }
