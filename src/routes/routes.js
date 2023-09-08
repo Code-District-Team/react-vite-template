a04e41cc0bb250accd684c6c0cc755e1297b9ae4
Haseeb Awan <haseeb.awan@codedistrict.com>
2023-09-08 19:15:22 +0500

    routes setup

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -14,6 +14,7 @@ import Users from "~/features/users/users";
 import GuestPageLayout from "~/layout/guestPageLayout";
 import LoggedInPageLayout from "~/layout/loggedInPageLayout";
 import K from "~/utilities/constants";
+import CreateUser from "~/features/users/createUser";
 
 /* 
   * Template for a route
@@ -83,6 +84,14 @@ const routes = [
     path: "/users",
     name: "Users",
     component: Users,
+    authenticated: true,
+    permission: null,
+    layout: LoggedInPageLayout,
+  },
+  {
+    path: "/users/create",
+    name: "Create",
+    component: CreateUser,
     authenticated: true,
     permission: null,
     layout: LoggedInPageLayout,
