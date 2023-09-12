125dea1e8fa627912fedad628d763b205db4624c
Usama Mehmood <usama.mehmood@codedistrict.com>
2023-09-12 13:47:28 +0500

    Remove moment, used dayjs instead

--- src/layout/footer.jsx
+++ src/layout/footer.jsx
@@ -1,12 +1,12 @@
 import { Layout } from "antd";
-import moment from "moment";
+import dayjs from "dayjs";
 
 export default function Footer() {
   const { Footer } = Layout;
 
   return (
     <Footer style={{ textAlign: "center" }}>
-      React Template &copy;{moment().year()} Created by{" "}
+      React Template &copy;{dayjs().year()} Created by{" "}
       <a href="https://www.codedistrict.com/" target="_blank" rel="noreferrer">
         Code District
       </a>
