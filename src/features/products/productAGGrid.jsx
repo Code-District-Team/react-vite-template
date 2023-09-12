f95e2f4db1a55a169456364aa44d92a30854523c
Haseeb Awan <haseeb.awan@codedistrict.com>
2023-09-12 19:45:48 +0500

    fixes for AG Grid table

--- src/features/products/productAGGrid.jsx
+++ src/features/products/productAGGrid.jsx
@@ -1,49 +1,70 @@
 import { AgGridReact } from "ag-grid-react";
+import "ag-grid-enterprise";
 import "ag-grid-community/styles/ag-grid.css";
 import "ag-grid-community/styles/ag-theme-alpine.css";
-import { useEffect, useMemo, useRef, useState } from "react";
+import { useMemo, useRef, useState } from "react";
 import ProductModal from "./productModal";
-import { Button, Form, Input, message } from "antd";
-import { debounce } from "lodash";
+import { Button, Form, message } from "antd";
+// import { debounce } from "lodash";
 import { setFieldErrorsFromServer } from "~/utilities/generalUtility";
 import Product from "~/models/product";
 // import { current } from "@reduxjs/toolkit";
 const ProductAGGrid = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [form] = Form.useForm();
-  const searchParams = new URLSearchParams(location.search); // Create a URLSearchParams object with the current query string
-  const limit = searchParams.get("limit"); // Get 'limit' parameter
-  const page = searchParams.get("page"); // Get 'page' parameter
   const editId = useRef(null);
-  const [productData, setProductData] = useState({ products: [], total: 0 });
-  const [searchQuery, setSearchQuery] = useState("");
-  const [currentPage, setCurrentPage] = useState(1);
+  // const [productData, setProductData] = useState({ products: [], total: 0 });
+  // const [searchQuery, setSearchQuery] = useState("");
+  // const [currentPage, setCurrentPage] = useState(1);
   const [productId, setProductId] = useState(null);
 
   const fetchProductDetails = async (queryParams) => {
     try {
       // const response = await Product.getProductData(limit, page);
       const response = await Product.getProductData(queryParams);
-
-      setProductData(response.data);
+      return response.data;
+      // setProductData(response.data);
     } catch (error) {
       setFieldErrorsFromServer(error);
     }
   };
-
-  useEffect(() => {
-    fetchProductDetails(createQuery(1, 100));
-    console.log(currentPage);
-  }, [limit, page]);
+  const sortDict = {
+    asc: "ASC",
+    desc: "DESC",
+  };
+  const datasource = {
+    async getRows(params) {
+      console.log(params.request);
+      const { startRow, endRow } = params.request;
+      const limit = endRow - startRow;
+      const value = startRow / limit + 1;
+      const query = createQuery(
+        value,
+        limit,
+        params.request.sortModel[0]?.colId,
+        params.request.sortModel.length > 0
+          ? sortDict[params.request.sortModel[0]["sort"]]
+          : null,
+      );
+      const data = await fetchProductDetails(query);
+      params.success({
+        rowData: data.products,
+        rowCount: data.total,
+      });
+    },
+  };
+  const onGridReady = (params) => {
+    params.api.setServerSideDatasource(datasource);
+  };
 
   const createQuery = (page, limit, sortBy, sortOrder) => {
     let queryParam = `page=${page}&limit=${limit}`;
     if (sortBy) {
       queryParam += `&sortBy=${sortBy}&sortOrder=${sortOrder}`;
     }
-    if (searchQuery) {
-      queryParam += `&query=${searchQuery}`;
-    }
+    // if (searchQuery) {
+    //   queryParam += `&query=${searchQuery}`;
+    // }
     return queryParam;
   };
 
@@ -79,17 +100,17 @@ const ProductAGGrid = () => {
     form.resetFields();
   };
 
-  const handleSearch = async (e) => {
-    try {
-      const query = e.target.value;
-      setSearchQuery(query);
-      const queryParams = `limit=${10}&page=${1}&query=${query}`; //l or whatever your default values are
-      fetchProductDetails(queryParams);
-      setCurrentPage(1);
-    } catch (error) {
-      setFieldErrorsFromServer(error);
-    }
-  };
+  // const handleSearch = async (e) => {
+  //   try {
+  //     const query = e.target.value;
+  //     setSearchQuery(query);
+  //     const queryParams = `limit=${10}&page=${1}&query=${query}`; //l or whatever your default values are
+  //     fetchProductDetails(queryParams);
+  //     // setCurrentPage(1);
+  //   } catch (error) {
+  //     setFieldErrorsFromServer(error);
+  //   }
+  // };
 
   const handleButtonDelete = async (id) => {
     console.log("id here", id);
@@ -182,7 +203,7 @@ const ProductAGGrid = () => {
   //   }, []);
   return (
     <div className="ag-theme-alpine" style={{ height: 600, maxwidth: 100 }}>
-      <Input.Search
+      {/* <Input.Search
         allowClear
         className="mb-3"
         size="large"
@@ -199,23 +220,27 @@ const ProductAGGrid = () => {
         }}
       >
         Create Product
-      </Button>
+      </Button> */}
       <ProductModal
         isModalOpen={isModalOpen}
         handleCancel={handleCancel}
         form={form}
+        rowModelType="serverSide"
         onFinish={onFinish}
         editId={editId}
       />
       <AgGridReact
-        rowData={productData.products}
+        // rowData={productData.products}
         columnDefs={columnDefs}
         defaultColDef={defaultColDef}
         rowSelection="multiple"
         animateRows={true}
-        paginationGoToPage={currentPage}
         pagination={true}
         paginationPageSize={10}
+        cacheBlockSize={10}
+        onGridReady={onGridReady}
+        rowModelType={"serverSide"}
+        domLayout="autoHeight"
       />
     </div>
   );
