70dfefe458818e694e7d055a58f8739379fa134f
Haseeb Awan <haseeb.awan@codedistrict.com>
2023-09-25 19:51:30 +0500

    global search and filter implement in table

--- src/features/products/productAntd.jsx
+++ src/features/products/productAntd.jsx
@@ -1,7 +1,8 @@
-import { Button, Card, Form, Input, Table, message } from "antd";
+import { Button, Card, Form, Input, Space, Table, message } from "antd";
 import { debounce } from "lodash";
 import { useEffect, useRef, useState } from "react";
 import { useLocation } from "react-router-dom";
+import Highlighter from "react-highlight-words";
 // import { useDispatch } from "react-redux";
 // import { createLogger } from "vite";
 import Product from "~/models/product";
@@ -12,10 +13,11 @@ import {
   setFieldErrorsFromServer,
 } from "~/utilities/generalUtility";
 import ProductModal from "./productModal";
+import { SearchOutlined } from "@ant-design/icons";
 
 const ProductAntd = () => {
   const [productData, setProductData] = useState({ products: [], total: 0 });
-  const [searchQuery, setSearchQuery] = useState("");
+  // const [searchQuery, setSearchQuery] = useState("");
   const location = useLocation(); // useLocation hook to get the current location object
   const searchParams = new URLSearchParams(location.search); // Create a URLSearchParams object with the current query string
   const [form] = Form.useForm();
@@ -26,21 +28,154 @@ const ProductAntd = () => {
   const [productId, setProductId] = useState(null);
   const userRole = User.getRole();
   const editId = useRef(null);
-  const fetchProductDetails = async (queryParams) => {
-    try {
-      // const response = await Product.getProductData(limit, page);
-      const response = await Product.getProductData(queryParams);
+  const [searchText, setSearchText] = useState("");
+  const [searchedColumn, setSearchedColumn] = useState("");
+  const searchInput = useRef(null);
 
+  useEffect(() => {
+    const fetchData = async () => {
+      await fetchProductDetails();
+    };
+    fetchData();
+  }, [limit, page]);
+
+  const fetchProductDetails = async (values = {}) => {
+    console.log("values", values);
+    const body = {
+      page: values.page || 1,
+      limit: values.limit || 100,
+      query: values.searchQuery || "",
+    };
+    // Conditionally adding sortBy and sortOrder to the body
+    if (values.sortBy) {
+      body.sortBy = values.sortBy;
+    }
+    if (values.sortOrder) {
+      body.sortOrder = values.sortOrder;
+    }
+    try {
+      const response = await Product.getProductData(body);
+      console.log(response.data);
       setProductData(response.data);
     } catch (error) {
       setFieldErrorsFromServer(error);
     }
   };
 
-  useEffect(() => {
-    fetchProductDetails(createQuery(1, 10));
-  }, [limit, page]);
+  const handleQuickSearch = (selectedKeys, confirm, dataIndex) => {
+    confirm();
+    setSearchText(selectedKeys[0]);
+    setSearchedColumn(dataIndex);
+  };
+  const handleReset = (clearFilters) => {
+    clearFilters();
+    setSearchText("");
+  };
 
+  const getColumnSearchProps = (dataIndex) => ({
+    filterDropdown: ({
+      setSelectedKeys,
+      selectedKeys,
+      confirm,
+      clearFilters,
+      close,
+    }) => (
+      <div
+        style={{
+          padding: 8,
+        }}
+        onKeyDown={(e) => e.stopPropagation()}
+      >
+        <Input
+          ref={searchInput}
+          placeholder={`Search ${dataIndex}`}
+          value={selectedKeys[0]}
+          onChange={(e) =>
+            setSelectedKeys(e.target.value ? [e.target.value] : [])
+          }
+          onPressEnter={() =>
+            handleQuickSearch(selectedKeys, confirm, dataIndex)
+          }
+          style={{
+            marginBottom: 8,
+            display: "block",
+          }}
+        />
+        <Space>
+          <Button
+            type="primary"
+            onClick={() => handleQuickSearch(selectedKeys, confirm, dataIndex)}
+            icon={<SearchOutlined />}
+            size="small"
+            style={{
+              width: 90,
+            }}
+          >
+            Search
+          </Button>
+          <Button
+            onClick={() => clearFilters && handleReset(clearFilters)}
+            size="small"
+            style={{
+              width: 90,
+            }}
+          >
+            Reset
+          </Button>
+          <Button
+            type="link"
+            size="small"
+            onClick={() => {
+              confirm({
+                closeDropdown: false,
+              });
+              setSearchText(selectedKeys[0]);
+              setSearchedColumn(dataIndex);
+            }}
+          >
+            Filter
+          </Button>
+          <Button
+            type="link"
+            size="small"
+            onClick={() => {
+              close();
+            }}
+          >
+            close
+          </Button>
+        </Space>
+      </div>
+    ),
+    filterIcon: (filtered) => (
+      <SearchOutlined
+        style={{
+          color: filtered ? "#1677ff" : undefined,
+        }}
+      />
+    ),
+    onFilter: (value, record) =>
+      record[dataIndex].toString().toLowerCase().includes(value.toLowerCase()),
+    onFilterDropdownOpenChange: (visible) => {
+      if (visible) {
+        setTimeout(() => searchInput.current?.select(), 100);
+      }
+    },
+    render: (text) =>
+      searchedColumn === dataIndex ? (
+        <Highlighter
+          highlightStyle={{
+            backgroundColor: "#ffc069",
+            padding: 0,
+          }}
+          searchWords={[searchText]}
+          autoEscape
+          textToHighlight={text ? text.toString() : ""}
+        />
+      ) : (
+        text
+      ),
+  });
   const Columns = [
     {
       title: "ID",
@@ -51,27 +186,32 @@ const ProductAntd = () => {
       title: "Product Name",
       dataIndex: "name",
       sorter: true,
+      ...getColumnSearchProps("name"),
       render: (text) => <a>{text}</a>,
     },
     {
       title: "Price",
       dataIndex: "price",
       sorter: true,
+      ...getColumnSearchProps("price"),
     },
     {
       title: "Quantity",
       dataIndex: "quantity",
       sorter: true,
+      ...getColumnSearchProps("quantity"),
     },
     {
       title: "Created At",
       dataIndex: "createdAt",
       sorter: true,
+      ...getColumnSearchProps("createdAt"),
     },
     {
       title: "Updated At",
       dataIndex: "updatedAt",
       sorter: true,
+      ...getColumnSearchProps("updatedAt"),
     },
     {
       title: "Action",
@@ -106,40 +246,37 @@ const ProductAntd = () => {
     if (sortBy) {
       queryParam += `&sortBy=${sortBy}&sortOrder=${sortOrder}`;
     }
-    if (searchQuery) {
-      queryParam += `&query=${searchQuery}`;
-    }
+    // if (searchQuery) {
+    //   queryParam += `&query=${searchQuery}`;
+    // }
     return queryParam;
   };
-  const sortDict = {
-    ascend: "ASC",
-    descend: "DESC",
-  };
-  const onPageChange = (pagination, filter, sort) => {
+  // const sortDict = {
+  //   ascend: "ASC",
+  //   descend: "DESC",
+  // };
+  const onPageChange = (pagination, filters, sorter) => {
     const { pageSize, current } = pagination;
-    let queryParam = "";
-    console.log("sort", sort);
-    if (!sort.column) {
-      queryParam = createQuery(current, pageSize);
-    } else {
-      queryParam = createQuery(
-        current,
-        pageSize,
-        sort.field,
-        sortDict[sort.order],
-      );
+    const params = { page: current, limit: pageSize };
+    if (sorter.field) {
+      params.sortBy = sorter.field;
     }
-    fetchProductDetails(queryParam);
+    if (sorter.order) {
+      params.sortOrder = sorter.order === "ascend" ? "ASC" : "DESC";
+    }
+    fetchProductDetails(params);
     setCurrentPage(current);
   };
+
   const handleSearch = async (e) => {
     try {
       const query = e.target.value;
-      setSearchQuery(query);
-
-      const queryParams = `limit=${10}&page=${1}&query=${query}`; //l or whatever your default values are
-      fetchProductDetails(queryParams);
-      setCurrentPage(1);
+      console.log("query", query);
+      fetchProductDetails({
+        page: 1,
+        limit: 100,
+        searchQuery: query,
+      });
     } catch (error) {
       setFieldErrorsFromServer(error);
     }
