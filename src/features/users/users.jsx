d76ed6fe32f3a3993062ee62f129b51e072d761b
Usama Mehmood <usama.mehmood@codedistrict.com>
2023-09-28 19:14:22 +0500

    Remove card

--- src/features/users/users.jsx
+++ src/features/users/users.jsx
@@ -1,10 +1,5 @@
-import { Card } from "antd";
 import TableList from "~/common/tableList/tableList";
 
 export default function User() {
-  return (
-    <Card className="m-3">
-      <TableList />
-    </Card>
-  );
+  return <TableList />;
 }
