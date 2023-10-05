393e40f8b72e4ebd864ef59db318516a3c240729
Usama Mehmood <usama.mehmood@codedistrict.com>
2023-10-05 16:12:20 +0500

    shouldLoad

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -20,8 +20,8 @@ export default function App() {
     ) {
       const userRoles = User.getRole();
       dispatch(setPermissionsHash(userRoles));
-      setShouldLoad(true);
     }
+    setShouldLoad(true);
   }, []);
 
   if (!shouldLoad) return <Spinner />;
