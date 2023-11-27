b0ccf7aae1cb87b1cbfb8df0658074e4555e79d7
Usama Mehmood <usama.mehmood@codedistrict.com>
2023-11-27 11:18:56 +0500

    Updated to yarn v4 & vite v5

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
-import eslint from "vite-plugin-eslint";
 import { resolve } from "path";
+import { defineConfig, loadEnv } from "vite";
+import eslint from "vite-plugin-eslint";
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), "");
@@ -9,18 +9,16 @@ export default defineConfig(({ mode }) => {
   return {
     define: {
       __APP_ENV__: env.APP_ENV,
-      // global: {},
-      /**
-       * Uncomment this if you receive global not defined error
-       * OR
-       * Add `window.global ||= window;` in main.jsx
-       */
     },
     plugins: [react(), eslint()],
     resolve: {
       alias: {
         "~": resolve(__dirname, "./src"),
       },
+    },
+    preview: {
+      host: "localhost",
+      port: 4173, // Default port for build preview
     },
     server: {
       open: true,
