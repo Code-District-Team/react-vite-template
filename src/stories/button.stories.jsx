4e98cf0f76f9d0ced1eebc88efbdfbcefec85f23
Usama Mehmood <usama.mehmood@codedistrict.com>
2023-11-30 17:12:24 +0500

    30 november: meeting

---
+++ src/stories/button.stories.jsx
@@ -0,0 +1,10 @@
+import { LocalFiltersAntd } from "./LocalFiltersAntd";
+
+export default {
+  title: "Features/Button",
+  component: LocalFiltersAntd,
+  parameters: {},
+  //   tags: ["autodocs"],
+};
+
+export const AntdButton = {};
